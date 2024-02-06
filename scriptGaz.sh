3d3f9367e774e27e9c3d6eac942e3f736995762b
Ilan <106468200+ilanouk@users.noreply.github.com>
2024-02-06 14:34:20 +0100

    v2

--- scriptGaz.sh
+++ scriptGaz.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-output_dir="C:\Users\iland\OneDrive\Bureau\M1 2023\Administration Linux\TD\TP"
+output_dir="/home/u/Bureau/linux"
 
 current_datetime=$(date +"%Y%m%d")
 output_filename="consommation_gaz_${current_datetime}.json"
 
-api_url="https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/consommation-nationale-horaire-de-gaz-donnees-provisoires-grtgaz-terega-v2/records?select=date%2C%20consommation_journaliere_mwh_pcs&where=date%3E2023&order_by=date&limit=100&exclude=statut%3AProvisoire"
+api_url="https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/consommation-nationale-horaire-de-gaz-donnees-provisoires-grtgaz-terega-v2/records?select=date%2C%20consommation_journaliere_mwh_pcs&where=date%20%3E%202023&order_by=date%20DESC&limit=1&offset=1&exclude=statut%3AProvisoire"
 
-curl -o "${output_dir}/${output_filename}" "${api_url}"
+curl --http1.1 -o "${output_dir}/${output_filename}" "${api_url}"
 
 echo "Données téléchargées avec succès dans ${output_dir}/${output_filename}"
