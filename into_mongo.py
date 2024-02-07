63e4c07d7906d779ed116c0da657d1cde93cca28
Tigran <tigran.wattrelos@gmail.com>
2024-02-07 18:10:16 +0100

    tkt

--- into_mongo.py
+++ into_mongo.py
@@ -1,30 +1,20 @@
 import pymongo
 import os
 import json
-PATH_CURRENCY = "/home/tigran/Documents/Cours/linux/Projet_linux/conso_electrique"
+PATH_ELECT = "/home/tigran/Documents/Cours/linux/Projet_linux/conso_electrique"
 
+# Définition de la DB
+DB_NAME = "projet_linux"
+COLUMN = "conso_elect"
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient[DB_NAME]
+mycol = mydb[COLUMN]
+#Ajout des infos dans la DB
+documents = os.listdir(PATH_ELECT)
+# Ouverture de tout les fichiers et ajout dans la DB
 
-# DB_NAME = "test"
-# COLUMN = "customers"
-# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-# mydb = myclient[DB_NAME]
-
-# mycol = mydb[COLUMN]
-
-# mydict = { "name": "John", "address": "Highway 37" }
-
-# #x = mycol.insert_one(mydict)
-# x = mycol.find_one()
-
-# print(x) 
-# --------------------------- Brouillon -----------------------
-
-
-documents = os.listdir(PATH_CURRENCY)
-# Ouverture de tout les fichiers
 for path_doc in documents:
-    fichier = open(PATH_CURRENCY+"/"+path_doc)
-    fichier = json.load(fichier)
-
-    print(fichier["results"][0]['consommation'])
+    fichier = open(PATH_ELECT+"/"+path_doc)
+    fichier_json = json.load(fichier)
+    x = mycol.insert_one(fichier_json["results"][0])
+    os.remove(PATH_ELECT+"/"+path_doc)
