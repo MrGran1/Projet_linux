6c1c8e9cbc2f3af82db3dd7257f998fbe9a53865
Mahyr <106538282+MahyrF@users.noreply.github.com>
2024-02-07 19:32:19 +0100

    Update resume.py

--- resume.py
+++ resume.py
@@ -6,7 +6,6 @@ import json
 dbname="user"
 collection="Meteo"
 dossier_donnees = './meteo/'
-fichier_log = 'log.txt'
 
 
 client = MongoClient('localhost', 27017)
@@ -17,6 +16,7 @@ col = db[collection]
 def lire_donnees(dossier_donnees):
 
     files = os.listdir(dossier_donnees)
+    files.sort()
 
     #On parcourt les fichiers du repertoire
     for fichier in files:
@@ -29,16 +29,13 @@ def lire_donnees(dossier_donnees):
                 #On charger les donnees JSON depuis le fichier
                 fichier_json = json.load(file)
 
-                #Ouvre le fichier log en mode écriture (historique)
-                chemin_log= os.path.join(dossier_donnees, fichier_log)
-                with open(chemin_log, 'w') as file:
-                    # Utilise la fonction print pour écrire dans le fichier
-                    print("Date:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "- Le fichier", fichier ,"contient", len(fichier_json['results']), "éléments.\n", file=file)
-                    print("\nLe fichier", fichier ,"contient", len(fichier_json['results']), "éléments.")
+                print("\nLe fichier", fichier ,"contient", len(fichier_json['results']), "éléments.")
 
                 for result in fichier_json['results']:
                     resultat = {"numero_station": result["numero_station"], "date": result["date"], "nom": result["nom"], "departement": result["departement"],"code_postale": result["code_postale"], "longitude": result["longitude"], "latitude": result["latitude"], "altitude": result["altitude"], "temperature_min": result["temperature_min"], "temperature_max": result["temperature_max"], "temperature": result["temperature"], "humidite": result["humidite"], "pression": result["pression"]}
                     col.insert_one(resultat)
+                
+                print("\nLe fichier", fichier, "a été traité.")
 
             os.remove(chemin_fichier)
 
