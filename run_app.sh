1c0a4456ea0dc1a45c406b9abb0585eab1d2a830
Mahyr <106538282+MahyrF@users.noreply.github.com>
2024-02-24 17:10:50 +0100

    Update run_app.sh path

--- run_app.sh
+++ run_app.sh
@@ -4,7 +4,7 @@ export DISPLAY=:0
 
 pkill -SIGTERM firefox
 
-cd ~/Documents/Cours/linux/Projet_Linux/ &&
+cd ~/Documents/Cours/linux/Projet_linux/ &&
 
 (sleep 2 && /usr/bin/firefox "http://127.0.0.1:7177" &);
 
